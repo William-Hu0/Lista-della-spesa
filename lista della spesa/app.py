4be6f7a0a09ed2d47279c3e67204bc1f504a8428
William-Hu0 Hu <hu.william@iisgalvanimi.edu.it>
2025-04-04 08:20:55 +0000

    2

--- lista della spesa/app.py
+++ lista della spesa/app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+x="ciao"
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', paperino=x)
 
 if __name__ == '__main__':
     app.run(debug=True)
